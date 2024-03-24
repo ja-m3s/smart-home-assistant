d4142cd4105c0b668051e0df499d889857cf0518
ja-m3s <Redacted>
2024-03-24 02:46:18 +0000

    removed unncessary files

--- scripts/teardown-apps-off-k8s.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#   Name: teardown-apps-off-k8s.sh
-#   Description: Teardown applications from the K8S cluster
-#   Author: ja-m3s
-
-set -eux
-
-#Create all app components in k8s
-kubectl delete -f ../kubernetes
