c7f7f65b4bd2abf0060fe810facfcb10c0c3dc69
ja-m3s <ja-m3s>
2024-04-02 11:47:14 +0100

    refactor

--- scripts/build-images.sh
+++ scripts/build-images.sh
@@ -7,12 +7,17 @@ set -eux
 # Define script directory
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
-#Change to a repo of your choosing
-REPO=localhost:32000
+# Use the passed-in REPO variable if provided, otherwise fallback to default value
+REPO="${1:-localhost:32000}"
+
+# If no argument is passed, print out the default repository and a message indicating that an argument can be passed
+if [ $# -eq 0 ]; then
+    echo "No repository argument provided. Using default repository: ${REPO}"
+    echo "You can pass a custom repository as an argument. Example: ./build-images.sh mycustomrepo:5000"
+fi
 
 # Build the python-custom image
 docker build -t "${REPO}/python-custom:3.9.19-slim-bullseye"  "${SCRIPT_DIR}/../docker/python-custom"
-docker push ${REPO}/python-custom:3.9.19-slim-bullseye
+docker push "${REPO}/python-custom:3.9.19-slim-bullseye"
 
-#docker build -t "${REPO}/rabbitmq-custom:alpine"  "${SCRIPT_DIR}/../docker/event-bus"
-#docker push ${REPO}/rabbitmq-custom:alpine
+
