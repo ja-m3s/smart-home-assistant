7368d60f3af8086c039a60dd804f4347b0f9e570
ja-m3s <ja-m3s>
2024-04-02 11:22:34 +0100

    removed unnecessary variables, trying to fix deployment of event bus

--- scripts/local-dev-env-setup.sh
+++ scripts/local-dev-env-setup.sh
@@ -5,7 +5,7 @@ sudo apt install docker.io
 sudo usermod -a -G docker $USER
 
 #Microk8s
-sudo apt install microk8s
-microk8s enable hostpath-storage registry rbac cert-manager dashboard
+snap install microk8s --classic
+sudo microk8s enable hostpath-storage registry rbac cert-manager dashboard
 microk8s start
 microk8s status
