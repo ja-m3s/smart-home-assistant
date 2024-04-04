4bb029f818e4257a0709cf757f8e2fde1dd48d66
ja-m3s <ja-m3s>
2024-04-04 22:24:56 +0100

    started writing unit tests

--- java/lightBulb/src/test/java/lightBulb/LightBulbTest.java
+++ java/lightBulb/src/test/java/lightBulb/LightBulbTest.java
@@ -1,16 +1,40 @@
 package lightBulb;
 
 import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
+import org.junit.Before;
+import lightBulb.LightBulb.LightBulbState;
 
 public class LightBulbTest {
 
+    private LightBulb lightBulb;
+
+    @Before
+    public void setUp() {
+        lightBulb = new LightBulb();
+    }
 
     @Test
-    public void testConsumeQueue() throws Exception {
+    public void testInitialState() {
+        assertEquals(LightBulbState.ON.getValue(), lightBulb.getState());
+    }
 
-        //TODO Write the tests
-        assertEquals(true,true);
+    @Test
+    public void testSetState() {
+        lightBulb.setState(LightBulbState.OFF);
+        assertEquals(LightBulbState.OFF.getValue(), lightBulb.getState());
+    }
+
+    @Test
+    public void testToString() {
+        String expectedString = "LightBulb [state=ON]";
+        assertEquals(expectedString, lightBulb.toString());
+    }
+
+    @Test
+    public void testTimeTurnedOn() {
+        long currentTime = System.currentTimeMillis();
+        lightBulb.setTimeTurnedOn(currentTime);
+        assertEquals(currentTime, lightBulb.getTimeTurnedOn());
     }
 }
