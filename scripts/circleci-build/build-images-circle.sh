085f636c49515bfbb9ab4ba7f7e4800a39085023
ja-m3s <ja-m3s>
2024-04-04 23:05:45 +0100

    added code coverage storage

--- scripts/circleci-build/build-images-circle.sh
+++ scripts/circleci-build/build-images-circle.sh
@@ -7,16 +7,15 @@ set -eux
 # Define script directory
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
-REPO="${DOCKER_REPO}"
 
 docker login --username ${DOCKER_USER} --password ${DOCKER_PASSWORD}
 
 # Build the python-custom image
-docker build -t "${REPO}:db-importer-latest" "${SCRIPT_DIR}/../../java/dbImporter"
-docker push "${REPO}:db-importer-latest"
+docker build -t "${DOCKER_REPO}:db-importer-latest" "${SCRIPT_DIR}/../../java/dbImporter"
+docker push "${DOCKER_REPO}:db-importer-latest"
 
-docker build -t "${REPO}:light-bulb-latest" "${SCRIPT_DIR}/../../java/lightBulb"
-docker push "${REPO}:light-bulb-latest"
+docker build -t "${DOCKER_REPO}:light-bulb-latest" "${SCRIPT_DIR}/../../java/lightBulb"
+docker push "${DOCKER_REPO}:light-bulb-latest"
 
-docker build -t "${REPO}:light-bulb-monitor-latest" "${SCRIPT_DIR}/../../java/lightBulbMonitor"
-docker push "${REPO}:light-bulb-monitor-latest"
+docker build -t "${DOCKER_REPO}:light-bulb-monitor-latest" "${SCRIPT_DIR}/../../java/lightBulbMonitor"
+docker push "${DOCKER_REPO}:light-bulb-monitor-latest"
