4319ff3ebd6cba9258a4393b32534223412e21c6
ja-m3s <135831999+ja-m3s@users.noreply.github.com>
2024-04-05 18:00:24 +0000

    Update database-admin-console.sh

--- scripts/admin/database-admin-console.sh
+++ scripts/admin/database-admin-console.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# Name: database-admin-console.sh
+# Description: Connects up to the database for admin tasks
+# Author: ja-m3s
 
 microk8s kubectl exec -it cockroachdb-secure-client-0 -- \
     ./cockroach sql \
