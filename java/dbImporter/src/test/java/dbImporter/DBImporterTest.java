2998da97126a4677a21ab92e5c8b06cc9893922c
ja-m3s <ja-m3s>
2024-04-06 17:59:54 +0100

    edit test, removed helper method

--- java/dbImporter/src/test/java/dbImporter/DBImporterTest.java
+++ java/dbImporter/src/test/java/dbImporter/DBImporterTest.java
@@ -1,27 +1,11 @@
 package dbImporter;
 
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
 
-import java.lang.reflect.Field;
-import java.util.Map;
+import static org.junit.Assert.assertThrows;
 
 public class DBImporterTest {
 
-    @Test
-    public void testRetrieveEnvVariable_VariableExists_ReturnsValue() {
-        // Arrange
-        String variableName = "SOME_VARIABLE";
-        String expectedValue = "someValue";
-        setEnv(variableName, expectedValue);
-
-        // Act
-        String actualValue = DBImporter.retrieveEnvVariable(variableName);
-
-        // Assert
-        assertEquals(expectedValue, actualValue);
-    }
 
     @Test
     public void testRetrieveEnvVariable_VariableDoesNotExist_ThrowsException() {
@@ -34,16 +18,4 @@ public class DBImporterTest {
         });
     }
 
-    public static void setEnv(String key, String value) {
-        try {
-            Map<String, String> env = System.getenv();
-            Class<?> cl = env.getClass();
-            Field field = cl.getDeclaredField("m");
-            field.setAccessible(true);
-            Map<String, String> writableEnv = (Map<String, String>) field.get(env);
-            writableEnv.put(key, value);
-        } catch (Exception e) {
-            throw new IllegalStateException("Failed to set environment variable", e);
-        }
-    }
 }
