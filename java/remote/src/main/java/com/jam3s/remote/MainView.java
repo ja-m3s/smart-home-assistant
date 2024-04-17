e64a83f90c71ccdf5a1d8b95a799c84210208971
ja-m3s <ja-m3s>
2024-04-17 17:37:42 +0100

    refactor FE

--- java/remote/src/main/java/com/jam3s/remote/MainView.java
+++ java/remote/src/main/java/com/jam3s/remote/MainView.java
@@ -4,14 +4,16 @@ import java.io.IOException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.Date;
+
+import org.json.JSONObject;
 
 import com.vaadin.flow.component.DetachEvent;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.html.NativeLabel;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
@@ -41,14 +43,25 @@ public class MainView extends HorizontalLayout {
     
         // Iterate over the entries in the HashMap
         RemoteApplication.getLightBulbStatus().forEach((key, value) -> {
-            NativeLabel keyLabel = new NativeLabel(key); // Create a Label with the key
-            TextArea valueTextArea = new TextArea(); // Create a TextArea to display the message
-            valueTextArea.setValue(value.toString()); // Set the value of the TextArea
-            valueTextArea.setReadOnly(true); // Make the TextArea read-only
+            
+            //Get the value of the hashmap and put it into a JSONObject for easy parsing.
+            JSONObject msg = new JSONObject(value);
+
+            //Create some Textfields to hold our values
+            TextField lightBulbNameTextField = new TextField(key);
+            TextField bulbStatusTextField = new TextField(msg.get("bulb_status").toString());
+            Date timeTurnedOn = new Date(Long.parseLong(msg.get("time_turned_on").toString()));
+            TextField timeTurnedOnTextField = new TextField(timeTurnedOn.toString());
+
+            // Make the TextField read-only
+            bulbStatusTextField.setReadOnly(true); 
+            timeTurnedOnTextField.setReadOnly(true);
+            
+            // Add a button to turn lights on/off.
             Button toggleState = new Button("Toggle On/Off");
             toggleState.addClickListener(d -> handleToggleState(key));
-            // Add the Label and TextArea and Button to a HorizontalLayout
-            HorizontalLayout entryLayout = new HorizontalLayout(keyLabel, valueTextArea, toggleState);
+            // Add the Label and TextField and Button to a HorizontalLayout
+            HorizontalLayout entryLayout = new HorizontalLayout(lightBulbNameTextField, bulbStatusTextField,timeTurnedOnTextField, toggleState);
             lightBulbDisplayVLayout.add(entryLayout); // Add the HorizontalLayout to the VerticalLayout
         });
     }
