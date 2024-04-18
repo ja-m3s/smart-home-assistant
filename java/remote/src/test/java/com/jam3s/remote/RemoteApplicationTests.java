e1d89bb334e54c14ce7b836444fbb2e3d087d164
ja-m3s <ja-m3s>
2024-04-18 11:33:17 +0100

    Checkstyle

--- java/remote/src/test/java/com/jam3s/remote/RemoteApplicationTests.java
+++ java/remote/src/test/java/com/jam3s/remote/RemoteApplicationTests.java
@@ -2,17 +2,18 @@ package com.jam3s.remote;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class RemoteApplicationTests {
 
-	/**
-	 * Dummy Test.
-	 */
-	@Test
-	public void testDummy() {
-		assertEquals(1, 1);
-	}
+    /**
+     * Dummy Test.
+     */
+    @Test
+    public void testDummy() {
+        assertEquals(1, 1);
+    }
 
 }
