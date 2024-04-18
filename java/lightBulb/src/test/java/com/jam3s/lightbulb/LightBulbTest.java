e8851be50bd9df849a1c1228d5d53fd88cd44715
ja-m3s <ja-m3s>
2024-04-18 03:21:03 +0100

    Refactor to use gradle - fixing styles

--- java/lightBulb/src/test/java/com/jam3s/lightbulb/LightBulbTest.java
+++ java/lightBulb/src/test/java/com/jam3s/lightbulb/LightBulbTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.BeforeAll;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import com.jam3s.lightbulb.LightBulb.LightBulbState;
 
 public class LightBulbTest {
