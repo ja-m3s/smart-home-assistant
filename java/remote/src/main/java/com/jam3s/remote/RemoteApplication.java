e12fbfa1d061b0ed424f0f57575aaca9c7d821ca
ja-m3s <ja-m3s>
2024-04-18 11:37:55 +0100

    Checkstyle

--- java/remote/src/main/java/com/jam3s/remote/RemoteApplication.java
+++ java/remote/src/main/java/com/jam3s/remote/RemoteApplication.java
@@ -197,7 +197,7 @@ public final class RemoteApplication implements CommandLineRunner {
     }
 
     @Override
-    public void run(final String... args) {
+    public static void run(final String... args) {
         SharedUtils.setupRabbitMQConnection();
         hostname = SharedUtils.getEnvVar("HOSTNAME");
         System.out.println(hostname);
