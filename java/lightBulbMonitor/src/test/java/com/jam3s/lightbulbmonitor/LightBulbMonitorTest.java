e63f59ee3c7b3a5128a938f98e53ba0d12882512
ja-m3s <ja-m3s>
2024-04-18 01:37:44 +0100

    edits

--- java/lightBulbMonitor/src/test/java/com/jam3s/lightbulbmonitor/LightBulbMonitorTest.java
+++ java/lightBulbMonitor/src/test/java/com/jam3s/lightbulbmonitor/LightBulbMonitorTest.java
@@ -1,11 +1,10 @@
 package com.jam3s.lightbulbmonitor;
 
-import com.jam3s.sharedutils.SharedUtils;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
- 
+
 public class LightBulbMonitorTest {
 
     /**
@@ -15,7 +14,7 @@ public class LightBulbMonitorTest {
      */
     @Test
     public void testSendMessage() throws IOException, InterruptedException {
-        
+        //NYI
         assertEquals(2, Integer.sum(1, 1));
     }
 }
