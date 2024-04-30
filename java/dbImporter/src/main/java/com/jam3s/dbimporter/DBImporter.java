592f8fee8278a0e3074d92253693011f284188dc
ja-m3s <blank>
2024-04-30 17:30:05 +0100

    ssl

--- java/dbImporter/src/main/java/com/jam3s/dbimporter/DBImporter.java
+++ java/dbImporter/src/main/java/com/jam3s/dbimporter/DBImporter.java
@@ -168,7 +168,9 @@ public final class DBImporter {
 
                 // Construct JDBC connection string and establish connection
                 String connectionString = "jdbc:postgresql://" + dbHost + ":"
-                    + dbPort + "/" + dbName + "sslmode=verify-full&sslrootcert=/certs/ca.crt";
+                    + dbPort + "/" + dbName 
+                    + "?ssl=true" + "?sslmode=verify-full" + "?sslcert=certs/client.root.crt" 
+                    + "?sslkey=certs/client.root.key" + "?sslrootcert=certs/ca.crt";
 
                 //Connect
                 dbConnection = DriverManager.getConnection(connectionString, dbUser, dbPassword);
